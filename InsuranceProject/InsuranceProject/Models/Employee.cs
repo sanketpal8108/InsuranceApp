493e277d98070f2f186424fb89b90934e0d18742
Shreyas Chandrachoodan <shreyas.chandrachoodan@asl.mum.sst>
2023-11-06 11:12:07 +0530

    Migration file created

---
+++ InsuranceProject/InsuranceProject/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace InsuranceDay1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; } //LoginID
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
